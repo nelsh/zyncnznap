dc2f6b53b03bad966b2d1139126cc6735f6b0b89
nelsh <nik.elshanski@gmail.com>
2015-10-30 14:40:56 +0800

    add config file and read it

--- zs-check.go
+++ zs-check.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 func main() {
+	viper.SetConfigFile("zs.conf")
+	viper.SetConfigType("toml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 }
