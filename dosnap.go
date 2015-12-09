12fbfecd49a7c958d98c4b9bc74303bab666f392
nelsh <nik.elshanski@gmail.com>
2015-12-09 16:59:51 +0800

    add deleting snapshots

--- dosnap.go
+++ dosnap.go
@@ -24,20 +24,6 @@ type SnapTotals struct {
 }
 
 func dosnap() {
-	// calculate snapshot name
-	t := time.Now()
-	snapName := t.Format("20060102")
-	if t.Weekday() != time.Saturday {
-		snapName += "d"
-	} else {
-		_, weekNum := t.ISOWeek()
-		if weekNum%13 == 1 {
-			snapName += "q"
-		} else {
-			snapName += "w"
-		}
-	}
-
 	hostname := getHostName()
 	// check root backup path
 	if _, err := zfs.GetDataset(viper.GetString("ZfsPath")); err != nil {
@@ -49,13 +35,61 @@ func dosnap() {
 		}
 		os.Exit(1)
 	}
+	/* end check */
+
+	/*
+		MAIN PROCEDURE
+	*/
 	delimeter := func() string {
-		return "\n" + strings.Repeat("-", 40) + "\n"
+		return "\n" + strings.Repeat("-", 52) + "\n"
 	}
 	totals := SnapTotals{
-		report: fmt.Sprintf("%-21s | %7s | %12s |\n",
+		report: fmt.Sprintf("%-25s | %7s | %12s |",
 			"Group/Server/Dir", "New", "Delete"),
 	}
+	totals.report += delimeter()
+
+	// calculate snapshot name
+	getStorageTime := func(label string) int {
+		keyOfPeriod := "storageperiod." + label
+		if viper.IsSet(keyOfPeriod) {
+			storageTime := viper.GetInt(keyOfPeriod)
+			if storageTime > 0 {
+				log.Printf("INFO: '%s' = %d", keyOfPeriod, storageTime)
+			} else {
+				log.Printf("WARN: '%s' not set or zero.", keyOfPeriod)
+			}
+			return storageTime
+		}
+		log.Printf("WARN: '%s' not exist", keyOfPeriod)
+		return 0
+	}
+	t := time.Now()
+	var snapLabel string
+	storagePeriod := 0
+	if t.Weekday() != time.Saturday {
+		snapLabel = "d"
+		storagePeriod = getStorageTime("d")
+	} else {
+		_, weekNum := t.ISOWeek()
+		if weekNum%13 == 1 {
+			snapLabel = "q"
+			storagePeriod = getStorageTime("q")
+		} else {
+			snapLabel = "w"
+			storagePeriod = getStorageTime("w")
+		}
+	}
+	//snapName := t.Format("20060102") + snapLabel
+	snapName := t.Format("20060102150405") + snapLabel
+	var oldSnapName string
+	if storagePeriod == 0 {
+		oldSnapName = t.Add(-time.Hour*24*(365*10)).Format("20060102150405") + snapLabel
+	} else { // time.Second => time.Hour*24
+		oldSnapName = t.Add(-time.Second*time.Duration(storagePeriod)).Format("20060102150405") + snapLabel
+	}
+	log.Printf("INFO: 'newSnapName' = %s", snapName)
+	log.Printf("INFO: 'oldSnapName' = %s", oldSnapName)
 
 	// enumerate backups and check path
 	for group := range viper.GetStringMap("groups") {
@@ -72,6 +106,9 @@ func dosnap() {
 			logWarnTotals(&totals, msg)
 			continue
 		}
+		//
+		// enumerate servers
+		//
 		for server := range viper.GetStringMap("groups." + group + ".servers") {
 			zPath := path.Join(zPath, server)
 			if _, err := zfs.GetDataset(zPath); err != nil {
@@ -80,6 +117,9 @@ func dosnap() {
 				logWarnTotals(&totals, msg)
 				continue
 			}
+			//
+			// enumerate dirs
+			//
 			for dir := range viper.GetStringMap("groups." + group + ".servers." + server + ".dirs") {
 				zPath := path.Join(zPath, dir)
 				ds, err := zfs.GetDataset(zPath)
@@ -87,35 +127,70 @@ func dosnap() {
 					msg := fmt.Sprintf("    WARN: skip dir '%s/%s/%s', error: '%s'\n",
 						group, server, dir, err.Error())
 					logWarnTotals(&totals, msg)
-					totals.report += fmt.Sprintf("%-21s | %7s | %12s |\n",
+					totals.report += fmt.Sprintf("%-25s | %7s | %12s |\n",
 						fmt.Sprintf("%s/%s/%s", group, server, dir), "ERROR", "ERROR")
 					continue
 				}
-				// make snap
 				logSnapErrTotals := func(totals *SnapTotals, msg string) {
 					totals.ErrNum++
 					totals.ErrMsg += msg
 					log.Printf(msg)
 				}
 				totals.TotalDirs++
+				newSnapResult := "SKIP"
+				delSnapResult := "SKIP"
+				// make snap
 				if _, err := ds.Snapshot(snapName, false); err != nil {
 					msg := fmt.Sprintf("\tERROR: '%s/%s/%s', error: '%s'\n",
 						group, server, dir, err.Error())
 					logSnapErrTotals(&totals, msg)
-					totals.report += fmt.Sprintf("%-21s | %7s | %12s |\n",
-						fmt.Sprintf("%s/%s/%s", group, server, dir), "ERROR", "SKIP")
+					newSnapResult = "ERROR"
+					// goto end
 				} else {
+					// if snap without error
 					log.Printf("\tSNAP: '%s/%s/%s' = OK\n", group, server, dir)
-					// make destroy old snapshot
-					isDestroy := false
-					if isDestroy {
-						totals.report += fmt.Sprintf("%-21s | %7s | %12s |\n",
-							fmt.Sprintf("%s/%s/%s", group, server, dir), "OK", "snapname")
+					newSnapResult = "OK"
+					// storageTime set?
+					if storagePeriod == 0 {
+						delSnapResult = "Disabled"
+						//goto end
 					} else {
-						totals.report += fmt.Sprintf("%-21s | %7s | %12s |\n",
-							fmt.Sprintf("%s/%s/%s", group, server, dir), "OK", "SKIP")
+						// get snapShots
+						snapShots, err := zfs.Snapshots(zPath)
+						if err != nil {
+							msg := fmt.Sprintf("\tERROR: '%s/%s/%s', error: '%s'\n",
+								group, server, dir, err.Error())
+							logSnapErrTotals(&totals, msg)
+							delSnapResult = "ERROR"
+							//goto end
+						} else {
+							delSnapResult = "CHECK"
+							snapTotal := 0
+							snapDeleting := 0
+							snapDeleted := 0
+							for _, sn := range snapShots {
+								if strings.HasSuffix(sn.Name, snapLabel) {
+									snapTotal++
+									if zPath+"@"+oldSnapName > sn.Name {
+										snapDeleting++
+										if err := sn.Destroy(zfs.DestroyDefault); err != nil {
+											msg := fmt.Sprintf("\tERROR: '%s/%s/%s', error: '%s'\n",
+												group, server, dir, err.Error())
+											logSnapErrTotals(&totals, msg)
+										} else {
+											snapDeleted++
+											log.Printf("\t\tdeleting '%s' = OK", sn.Name)
+										}
+									}
+								}
+							}
+							delSnapResult = fmt.Sprintf("%d/%d/%d",
+								snapDeleted, snapDeleting, snapTotal)
+						}
 					}
 				}
+				totals.report += fmt.Sprintf("%-25s | %7s | %12s |\n",
+					fmt.Sprintf("%s/%s/%s", group, server, dir), newSnapResult, delSnapResult)
 			}
 		}
 	}
